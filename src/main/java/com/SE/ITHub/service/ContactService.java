2738845bd572c5e983a6fe8ca1f10c9bdb945545
IT24104152 <gayuthdemika@gmail.com>
2026-02-26 22:22:39 +0530

    FSIHS-74 FSIHS-137 feat(auth): Admin Login Using OAuth 2.0

--- src/main/java/com/SE/ITHub/service/ContactService.java
+++
@@ -1,9 +0,0 @@
-package com.SE.ITHub.service;
-
-import com.SE.ITHub.model.Contact;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ContactService {
-    Contact addContactMessage(Contact contact);
-}
