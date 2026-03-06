ece3c5fae637a4e00b48bef914e1e376a697fdd8
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-03-06 18:23:50 +0530

    FSIHS-87 FSIHS-218 fix: added more methods and replaced the data type with UUID

--- src/main/java/com/SE/ITHub/repository/PortfolioItemRepository.java
+++ src/main/java/com/SE/ITHub/repository/PortfolioItemRepository.java
@@ -3,5 +3,11 @@ package com.SE.ITHub.repository;
 import com.SE.ITHub.model.PortfolioItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PortfolioItemRepository extends JpaRepository<PortfolioItem, Integer> {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PortfolioItemRepository extends JpaRepository<PortfolioItem, UUID> {
+    Optional<PortfolioItem> findBySlug(String slug);
+    boolean existsBySlug(String slug);
+    boolean existsBySlugAndIdNot(String slug, UUID id);
 }
