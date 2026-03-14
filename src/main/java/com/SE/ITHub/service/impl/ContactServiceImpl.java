15bcf9a627cfb3b594c56f27a80e88621997f1d1
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-03-14 22:34:48 +0530

    fix: restore files missing from dev after merge

---
+++ src/main/java/com/SE/ITHub/service/impl/ContactServiceImpl.java
@@ -0,0 +1,27 @@
+package com.SE.ITHub.service.impl;
+
+import com.SE.ITHub.dto.ContactCreateRequest;
+import com.SE.ITHub.dto.ContactResponse;
+import com.SE.ITHub.exception.ContactNotFoundException;
+import com.SE.ITHub.mapper.ContactMapper;
+import com.SE.ITHub.model.Contact;
+import com.SE.ITHub.repository.ContactRepository;
+import com.SE.ITHub.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public Contact findById(UUID id){
+        return contactRepository.findById(id).orElseThrow(()->new ContactNotFoundException("Contact not found "+id));
+    }
+
+    public ContactResponse addContactMessage(ContactCreateRequest contact){
+        return  ContactMapper.toResponse(contactRepository.save(ContactMapper.toEntity(contact)));
+    }
+}
