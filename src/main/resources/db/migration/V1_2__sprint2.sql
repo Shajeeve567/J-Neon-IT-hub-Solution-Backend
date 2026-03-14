a333279422031d5fe413a18da589bc3507b0a231
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-03-14 22:57:34 +0530

    fix: small fix in the 2nd migration file

--- src/main/resources/db/migration/V1_2__sprint2.sql
+++ src/main/resources/db/migration/V1_2__sprint2.sql
@@ -1,3 +1,1 @@
-ALTER TABLE portfolio_images
-    ADD COLUMN image_url TEXT;
-    ALTER COLUMN media_id DROP NOT NULL;
+ALTER TABLE portfolio_images ADD COLUMN image_url TEXT; ALTER COLUMN media_id DROP NOT NULL;
