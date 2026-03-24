e8caa9f3070fdbe905a2ef21bd4767fba631576c
IT24104152 <gayuthdemika@gmail.com>
2026-03-24 19:37:07 +0530

    FSIHS-85 FSIHS-263 feat(edit-contact-info): adding memory tags to the contact messages in the admin dashboard
    - creates a PUT request method to update the contact
    - created ContactTagRequestDTO for secure mapping
    - Unit tests done

--- src/main/java/com/SE/ITHub/controller/ContactController.java
+++ src/main/java/com/SE/ITHub/controller/ContactController.java
@@ -1,6 +1,7 @@
 package com.SE.ITHub.controller;
 
 import com.SE.ITHub.dto.ContactCreateRequest;
+import com.SE.ITHub.dto.ContactTagRequest;
 import com.SE.ITHub.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +15,8 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @PostMapping("/post")
-    public ResponseEntity<?> postContact(@RequestBody ContactCreateRequest contact){
-        return ResponseEntity.status(HttpStatus.OK).body(contactService.addContactMessage(contact));
+    @PutMapping("/tags")
+    public ResponseEntity<?> editContact(@RequestBody ContactTagRequest contact){
+        return ResponseEntity.status(HttpStatus.OK).body(contactService.updateContactMessage(contact));
     }
 }
--- src/main/java/com/SE/ITHub/service/ContactService.java
+++ src/main/java/com/SE/ITHub/service/ContactService.java
@@ -2,11 +2,14 @@ package com.SE.ITHub.service;
 
 import com.SE.ITHub.dto.ContactCreateRequest;
 import com.SE.ITHub.dto.ContactResponse;
+import com.SE.ITHub.dto.ContactTagRequest;
 import com.SE.ITHub.model.Contact;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public interface ContactService {
-    Contact findById(Long id);
-    ContactResponse addContactMessage(ContactCreateRequest contact);
+    Contact findById(UUID id);
+    ContactResponse updateContactMessage(ContactTagRequest contact);
 }
