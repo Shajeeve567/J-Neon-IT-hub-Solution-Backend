d69e4d156500b4571b4e6547d042414b77beeb10
rithish279 <rajeswaranrithish@gmail.com>
2026-03-29 09:22:30 +0530

    FSIHS-88 FSIHS-228 feat(media): Implement Spring Boot Multipart File Upload Endpoint

---
+++ src/main/java/com/SE/ITHub/dto/MediaPushDto.java
@@ -0,0 +1,13 @@
+package com.SE.ITHub.dto;
+
+import java.time.LocalDateTime;
+
+public class MediaPushDto {
+    private String fileName;
+    private String altText;
+    private String caption;
+    private Integer width;
+    private Integer height;
+    private LocalDateTime uploadedAt;
+    private LocalDateTime createdAt;
+}
