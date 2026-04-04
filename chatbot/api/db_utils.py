ad239f09e6b5861aaf153c57edce6b48cb40b7bd
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-04-04 18:24:20 +0530

    FSIHS-90 FSIHS-242 feat: intial db connection file

---
+++ chatbot/api/db_utils.py
@@ -0,0 +1,11 @@
+import sqlite3
+from datetime import datetime
+
+DB_NAME = "rag_app.db"
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
