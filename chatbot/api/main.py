f728009433b22548e5d9192a34c1d97ef087cf98
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-04-04 16:13:11 +0530

    FSIHS-90 FSIHS-242 FSIHS-239 feat: Initial Project Structure

---
+++ chatbot/api/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, HTTPException, File
+import os
+import uuid
+import shutil
+
+
+app = FastAPI()
+
+
+def upload_document(file: UploadFile = File(...)):
+    allowed_extensions = ['.pdf', '.docx', '.html']
+    file_extension = os.path.splitext(file.filename)[1].lower()
+
+    if file_extension not in allowed_extensions:
+        raise HTTPException(status_code=400, detail=f"Unsupported file type. Allowed types are: {', '.join(allowed_extensions)}")
+    
+    unique_filename =  f"{uuid.uuid4()}_{file.filename}"
+    temp_file_path = os.path.join(os.getcwd(), unique_filename)
+
+    try:
+        # Save the uploaded file to a temporary file
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+    finally:
+        if os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
