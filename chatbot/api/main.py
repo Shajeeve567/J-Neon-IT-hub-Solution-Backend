291749c93e33e40700bbbf6c5d68d92ab6271411
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-04-04 16:26:08 +0530

    FSIHS-90 FSIHS-239 feat: added the document type validation code

--- chatbot/api/main.py
+++ chatbot/api/main.py
@@ -8,6 +8,12 @@ app = FastAPI()
 
 
 def upload_document(file: UploadFile = File(...)):
+    allowed_extensions = ['.pdf']
+    file_extension = os.path.splitext(file.filename)[1].lower()
+
+    if file_extension not in allowed_extensions:
+        raise HTTPException(status_code=400, detail=f"Unsupported file type. Allowed types are: {', '.join(allowed_extensions)}")
+    
     unique_filename =  f"{uuid.uuid4()}_{file.filename}"
     temp_file_path = os.path.join(os.getcwd(), unique_filename)
 
