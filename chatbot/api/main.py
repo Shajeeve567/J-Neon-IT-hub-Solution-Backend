54796b3400a4ce5f5a7527194a1200dff50eef94
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-04-04 16:21:47 +0530

    FSIHS-90 FSIHS-242 feat: added the first version of upload document function

--- chatbot/api/main.py
+++ chatbot/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, HTTPException, File
+from fastapi import FastAPI, UploadFile, HTTPException, Fi
 import os
 import uuid
 import shutil
@@ -6,3 +6,16 @@ import shutil
 
 app = FastAPI()
 
+
+def upload_document(file: UploadFile = File(...)):
+    unique_filename =  f"{uuid.uuid4()}_{file.filename}"
+    temp_file_path = os.path.join(os.getcwd(), unique_filename)
+
+    try:
+        # Save the uploaded file to a temporary file
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+    finally:
+        if os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
